ddd3494763ad05fded9cbcb7ebad9838516d8aab
Kelly <128594606+itskellyturner@users.noreply.github.com>
2023-09-22 20:57:58 +0200

    Create contactmesection.js

---
+++ src/component/contactmesection.js
@@ -0,0 +1,135 @@
+import React, { useEffect } from "react";
+import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+import useSubmit from "../Hooks/useSubmit.js";
+import { useAlertContext } from "../context/alertContext";
+
+const initialValues = {
+  firstName: "",
+  email: "",
+  type: "hireMe",
+  comment: "",
+};
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.comment) {
+    errors.comment = "Required";
+  } else if (values.comment.length < 25) {
+    errors.comment = "Must be at least 25 characters";
+  }
+
+  return errors;
+};
+
+const LandingSection = () => {
+  const { isLoading, response, submit } = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  const formik = useFormik({
+    initialValues,
+    validate,
+    onSubmit: (values, actions) => {
+      submit("https://api.receive.com", values).then(() => {
+        actions.resetForm(initialValues);
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (response) {
+      onOpen(response.type, response.message);
+    }
+  }, [response]);
+
+  return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#512DA8"
+      py={16}
+      spacing={8}
+    >
+      <VStack w="1024px" p={32} alignItems="flex-start">
+        <Heading as="h1" id="contactme-section">
+          Contact me
+        </Heading>
+        <Box p={6} rounded="md" w="100%">
+          <form onSubmit={formik.handleSubmit}>
+            <VStack spacing={4}>
+              <FormControl isInvalid={formik.touched.firstName && formik.errors.firstName}>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  {...formik.getFieldProps("firstName")}
+                />
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  {...formik.getFieldProps("email")}
+                />
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select id="type" name="type" {...formik.getFieldProps("type")}>
+                  <option value="hireMe">Freelance project proposal</option>
+                  <option value="openSource">Open source consultancy session</option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+              <FormControl isInvalid={formik.touched.comment && formik.errors.comment}>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  {...formik.getFieldProps("comment")}
+                />
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+              </FormControl>
+              <Button
+                type="submit"
+                colorScheme="purple"
+                width="full"
+                isLoading={isLoading}
+              >
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </FullScreenSection>
+  );
+};
+
+export default LandingSection;
